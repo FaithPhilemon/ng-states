72cf54a2927dfddb18fd3ed7542e0f95cc6fcdab
Faith Philemon <philemonfaith@gmail.com>
2018-08-28 01:26:51 +0100

    create db class

---
+++ Db.php
@@ -0,0 +1,30 @@
+<?php
+    class Db
+    {
+        private $host = "localhost";
+        private $db_name = "ngp";
+        private $username = "root";
+        private $password = "";
+        public $conn;
+    
+        // get the database connection
+        public function getConnection(){
+    
+            $this->conn = null;
+    
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            }catch(PDOException $exception){
+                echo "Connection error: " . $exception->getMessage();
+            }
+    
+            return $this->conn;
+        }
+    }
+    
+
+
+
+
+
+?>
