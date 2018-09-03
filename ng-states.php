a8b0fe7e785230197a00f4b5059c8bd9f55ec3c2
Faith Philemon <philemonfaith@gmail.com>
2018-09-03 10:22:41 +0100

    create get_lgs()

--- ng-states.php
+++ ng-states.php
@@ -17,4 +17,27 @@
         }
         
     }
+
+    function get_lgs($state_id){
+        include ("Db.php"); 
+        if($state_id == ""){
+            $sql = "SELECT * FROM `locals` ORDER BY `locals`.`state_id` ASC";
+        }else {
+            $sql = "SELECT * FROM `locals` WHERE state_id = '$state_id' ORDER BY `locals`.`state_id` ASC";
+        }
+        
+        $result = mysqli_query($con, $sql);
+        if(!$result){
+
+            die("ERROR OCCURED ". mysqli_error($con));
+
+        }else {
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $lgs[] = mysqli_fetch_assoc($result);
+            }
+            return $lgs;
+        }
+        
+    }
 ?>
